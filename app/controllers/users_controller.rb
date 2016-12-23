2a9917b29bb430c3e41281118361519797bf7acc
原ひかり <hikari.hara18@gmail.com>
2016-12-23 21:39:14 +0900

    アカウントページの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
-      @nickname = current_user.nickname
-      @nikkis = current_user.nikkis.page(params[:page]).per(5).order("created_at DESC")
+      # @nickname = current_user.nickname
+      @user = User.find(params[:id])
+      # @nikkis = nikkis.page(params[:page]).per(5).order("created_at DESC")
+
   end
 end
